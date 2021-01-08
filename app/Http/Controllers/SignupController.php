e1d81ae727e1dc2edcd4561913598f0193f9a1d5
quynhnhi0124 <53897670+quynhnhi0124@users.noreply.github.com>
2021-01-08 13:23:42 +0700

    complete with login function

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Users;
 use Illuminate\Http\Request;
 use App\Http\Requests\Signup;
+use Illuminate\Support\Facades\Hash;
+
 //use App\Models\Users;
 
 class SignupController extends Controller
@@ -15,8 +17,11 @@ class SignupController extends Controller
 
     public function store(Signup $request){
         $validated = $request->validated();
-        $users = Users::create(request(['name','email','password']));
-        return redirect()->to('/signup');
+        $users = Users::create(([
+            'name'=>$request['name'],
+            'email'=>$request['email'],
+            'password'=>Hash::make($request['password'])]));
+        return redirect()->to('/home/signup');
 
     }
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 <title>Homepage</title>
 @section('content')
-@if(Route::has('login'))
 Hi {{Auth::user()->name}}
-@endif
 @endsection
